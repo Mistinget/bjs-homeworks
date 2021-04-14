9ca7aed43fa632d3ae61b19fa9f210861289a022
Anna Boitsova <kalinda-k@mail.ru>
2021-04-14 22:55:20 +0300

    First commit

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,20 +1,36 @@
-"use strikt"
+"use strikt";
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    console.log(typeof percent, typeof contribution, typeof amount, date.valueOf);
-    if (typeof percent === undefined || percent <= 0) {
-        return `Некорректный ввод ${percent}`;
-    } else if (typeof contribution === undefined || typeof contribution <= 0) {
-        return `Некорректный ввод ${contribution}`;
-    } else if (typeof amount === undefined || typeof amount <= 0) {
-        return `Некорректный ввод ${amount}`;
-    } //else if (typeof date === undefined || typeof date <= 0) {
-    //return `Некорректный ввод ${date}`;                      // ????
+    console.log(typeof percent, typeof contribution, typeof amount);
+    if (typeof percent === "undefined" || Number(percent) <= 0) {
+        return `Некорректный ввод "Процентная ставка" ${percent}`;
+    } else if (typeof contribution === "undefined" || Number(contribution) < 0) {
+        return `Некорректный ввод "Начальный взнос" ${contribution}`;
+    } else if (typeof amount === "undefined" || Number(amount) <= 0) {
+        return `Некорректный ввод "Общая стоимость" ${amount}`;
+    }
 
-    // return totalAmount;
+    let P = percent / 100 / 12; // 1/12 процентной ставки
+    let today = new Date;
+    console.log(date.getFullYear(), today.getFullYear());
+    //кол-во выплач. месяцев:
+    let amountMonth = ((date.getFullYear() - today.getFullYear()) * 12 + (date.getMonth() - today.getMonth()));
+    console.log(amountMonth);
+    let S = amount - contribution; //тело кредита
+    console.log(S, P);
+    let monthPay = S * (P + P / (((1 + P) ** amountMonth) - 1));
+    console.log(monthPay);
+    let totalAmount = (monthPay * amountMonth);
+    console.log(totalAmount.toFixed(2));
+    console.log(typeof totalAmount);
+    return Number(totalAmount.toFixed(2));
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
-    // return greeting;
+    if (!name || typeof name === "undefined" || Boolean(name) === false) {
+        name = "Аноним";
+    }
+    console.log("Привет, мир! Меня зовут ${name}.", typeof name, name.length);
+    console.log(Boolean(name));
+    return `Привет, мир! Меня зовут ${name}.`;
 }
